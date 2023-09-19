61401492eb11d6c96621bc83a6266c54518b0bc5
sesurya <chakkapalli.suryateja@byjus.com>
2023-09-19 22:58:07 +0530

    Added some more features

--- src/home/index.js
+++ src/home/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './style.css'; // Import CSS file for styling
 import { useTypewriter,Cursor } from 'react-simple-typewriter';
+import {Row,Col} from 'react-bootstrap';
 
 const HomePage = () => {
   const [text] = useTypewriter({
@@ -9,18 +10,21 @@ const HomePage = () => {
   });
   return (
     <div className="home-page">
-      <div className="content">
-        <h1> Hi I'm  {' '}
-            <span style={{fontWeight:'bold',color:'green'}}>
-              {text}
-              </span>
-              <span>
-               <Cursor/>
-
-              </span>
-        </h1>
-        <div> Hi all my name is Surya, welcome to my tech-centric realm,<br/> where I share my research, interests, and insights through captivating blogs.<br/> Join me on this exhilarating journey of knowledge and passion."</div>
-      </div>
+      <Row>
+        <Col className='card-block-home' lg={6}>
+            <div className="content" >
+            <h1> Hi I'm  {' '}
+                <span style={{fontWeight:'bold',color:'green'}}>
+                  {text}
+                  </span>
+                  <span>
+                  <Cursor/>
+                  </span>
+            </h1>
+            <div> Hi all my name is Surya, welcome to my tech-centric realm,<br/> where I share my research, interests, and insights through captivating blogs.<br/> Join me on this exhilarating journey of knowledge and passion."</div>
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 };
